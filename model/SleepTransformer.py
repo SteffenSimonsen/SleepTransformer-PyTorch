1a1e215aca2d8c02b4735351ba1b42824b61accf
steffen <steffentorpe@gmail.com>
2024-07-15 20:42:25 +0200

    Got closer to finalizing the model

--- model/SleepTransformer.py
+++ model/SleepTransformer.py
@@ -1,5 +1,5 @@
-import torch 
 import torch.nn as nn
+from torch.nn import functional as F
 from modules import Attention
 from transformer_module import TransformerHeap
 
@@ -28,9 +28,53 @@ class SleepTransformer(nn.Module):
             dropout_rate=config.dropout_rate
         )
         
-        # fc layers
-        self.fc1 = nn.Linear(config.seq_d_model, config.fc_hidden_size)
-        self.fc2 = nn.Linear(config.fc_hidden_size, config.fc_hidden_size)
         
-        # Final classification layer
-        self.classifier = nn.Linear(config.fc_hidden_size, config.num_classes)
+        self.fc1 = nn.Linear(config.seq_d_model, config.fc_hidden_size) 
+        self.fc2 = nn.Linear(config.fc_hidden_size, config.num_classes) 
+
+    def forward(self, x):
+        
+        batch_size, epoch_seq_len, frame_seq_len, ndim, nchannel = x.shape # x: (batch_size, epoch_seq_len, frame_seq_len, ndim, nchannel)
+        
+        # reshape for epoch transformer
+        # combine batch_size and epoch_seq_len, and flatten ndim and nchannel
+        x = x.view(-1, frame_seq_len, ndim * nchannel) #  x : (batch_size * epoch_seq_len, frame_seq_len, ndim * nchannel)
+        
+        
+        #epoch transformer
+        epoch_output = self.epoch_transformer(x) # epoch_output : (batch_size * epoch_seq_len, frame_seq_len, d_model)
+        
+        
+        # attention mechanism
+        epoch_context = self.epoch_attention(epoch_output) # epoch_context : (batch_size * epoch_seq_len, d_model)
+       
+        
+        # reshape for sequence transformer
+        sequence_input = epoch_context.view(batch_size, epoch_seq_len, -1) # sequence_input : (batch_size, epoch_seq_len, d_model)
+        
+        
+        # sequence transformer
+        sequence_output = self.sequence_transformer(sequence_input) # sequence_output : (batch_size, epoch_seq_len, d_model)
+      
+        
+        #  FC layers
+        x = F.relu(self.fc1(sequence_output)) # x : (batch_size, epoch_seq_len, fc_hidden_size)
+        
+        logits = self.fc2(x) # logits : (batch_size, epoch_seq_len, num_classes)
+        
+        
+        # softmax
+        output = F.softmax(logits, dim=-1) # output : (batch_size, epoch_seq_len, num_classes)
+        
+        
+        return output
+    
+    def count_parameters(self):
+        """Count and print the number of parameters in each component and total."""
+        total_params = 0
+        for name, module in self.named_children():
+            params = sum(p.numel() for p in module.parameters() if p.requires_grad)
+            print(f"{name}: {params:,} parameters")
+            total_params += params
+        print(f"Total: {total_params:,} parameters")
+        return total_params
